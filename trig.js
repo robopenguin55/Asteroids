49051e4f94cc4d34622df6f51b06c80ff052b714
Jason Bowden <robopenguin55@gmail.com>
2023-05-26 09:34:04 -0600

    updated trig.js to accept circle size and color. Also added placeholders for how to implement projectiles.

--- trig.js
+++ trig.js
@@ -3,11 +3,11 @@ class Trigonometry {
         this.context = context;
     }
  
-    drawCircle(x, y) {
+    drawCircle(x, y, size=1, color='white') {
         this.context.beginPath();
-        this.context.arc(x,y,1,0,(2 * Math.PI), true);
-        this.context.strokeStyle = 'white';
-        this.context.fillStyle = 'white';
+        this.context.arc(x,y,size,0,(2 * Math.PI), true);
+        this.context.strokeStyle = color;
+        this.context.fillStyle = color;
         this.context.stroke();
     }
 
