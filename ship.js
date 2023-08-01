21acb9bd1dd1d1bd3c81fd4c6d9e92b809e0dc70
Jason Bowden <robopenguin55@gmail.com>
2023-07-31 21:11:00 -0600

    ship projectiles will now be removed when they leave the screen

--- ship.js
+++ ship.js
@@ -5,7 +5,7 @@ class Ship extends Projectile {
 
         this.shipSound = new sound("sounds/thrust.wav");
         this.fireSound = new sound("sounds/fire.wav");
-        this.shipImage = 'images/player.png'; //'spaceship-25.png';
+        this.shipImage = 'images/spaceship-25.png';//'images/player.png'; 
         this.projectiles = [];
 
         document.onkeyup = (e) => {
@@ -56,23 +56,41 @@ class Ship extends Projectile {
                 // use the current direction angle and current location to determine angle and origin of projectile
                 // add projectile to array of projectiles to be shown in game loop
                 //console.log(`drawing circle at x:${currentLocation[0]} y:${currentLocation[1]-10}`);
+
+                const velocityFactor = 15;
     
-                const v_x = -(Math.sin(this.currentDirectionAngle)) * 5;
-                const v_y = Math.cos(this.currentDirectionAngle) * 5;
+                const v_x = -(Math.sin(this.currentDirectionAngle)) * velocityFactor;
+                const v_y = Math.cos(this.currentDirectionAngle) * velocityFactor;
     
-                console.log(`starting at: x = ${this.currentLocation[0]} y = ${this.currentLocation[1]} v_x = ${v_x} v_y = ${v_y}`);
+                //console.log(`starting at: x = ${this.currentLocation[0]} y = ${this.currentLocation[1]} v_x = ${v_x} v_y = ${v_y}`);
     
-                this.projectiles.push(new Projectile(this.context, this.currentLocation[0],this.currentLocation[1], v_x, v_y)); // create new projectile
+                this.projectiles.push(new Projectile(this.context, this.currentLocation[0],this.currentLocation[1], v_x, v_y, false)); // create new projectile
             }
     
             //console.log(`x:${this.currentLocation[0]} y: ${this.currentLocation[1]} direction angle : ${this.currentDirectionAngle} velocity : ${this.currentVelocity[1]}`);
         };
     }
 
+    getShipLocation(){
+        return this.currentLocation;
+    }
+
+    getProjectileLocations(){
+        let matrix = [];
+        this.projectiles.forEach(a => matrix.push(a.currentLocation));
+        console.log(matrix);
+
+        return matrix;
+    }
+
     drawProjectiles(width, height) {
         for (let i = 0; i < this.projectiles.length; i++){
-            this.projectiles[i].updateLocation(width, height);
-            this.projectiles[i].draw(`${i}`,'images/projectile.png');
+            if (this.projectiles[i].updateLocation(width, height)){
+                this.projectiles[i].draw(`${i}`,'images/projectile.png');
+            }
+            else { // remove projectile when it is off the screen
+                this.projectiles.splice(i, 1);
+            }
         }
     }
 
